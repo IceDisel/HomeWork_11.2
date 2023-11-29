368f28df688e114705370c52c2289c2a50e78b5b
Vladimir Titov <tvv-vg@yandex.ru>
2023-11-29 19:54:36 +0300

    add update file

--- str_func.py
+++ str_func.py
@@ -1,2 +1,16 @@
 def get_upper_str(string):
+    """
+    Функция переводит слово в верхний регистр
+    :param string:
+    :return:
+    """
     return string.upper()
+
+
+def get_title_str(string):
+    """
+    Функция делает каждое слово с заглавной буквы
+    :param string:
+    :return:
+    """
+    return string.title()
